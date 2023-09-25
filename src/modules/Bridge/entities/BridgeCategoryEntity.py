f2a500a5449200008b1c7aa7fc17d06c21e4a670
contact@get-company.com <contact@get-company.com>
2023-09-25 16:13:48 +0200

    Created BridgeCategoryEntity and its relations

---
+++ src/modules/Bridge/entities/BridgeCategoryEntity.py
@@ -0,0 +1,67 @@
+from src import db
+import datetime
+
+# Definieren der Assoziationstabelle
+bridge_category_marketplace_association = db.Table(
+    'bridge_category_marketplace_association',
+    db.Column('category_id', db.Integer, db.ForeignKey('bridge_category_entity.id'), primary_key=True),
+    db.Column('marketplace_id', db.Integer, db.ForeignKey('bridge_marketplace_entity.id'), primary_key=True)
+)
+
+
+class TranslationWrapper:
+    def __init__(self, translation):
+        self.translation = translation
+
+    def __getattr__(self, name):
+        if self.translation:
+            return getattr(self.translation, name)
+        return None
+
+
+class BridgeCategoryEntity(db.Model):
+
+    __tablename__ = 'bridge_category_entity'
+
+    id = db.Column(db.Integer(), primary_key=True, nullable=False, autoincrement=True)
+    erp_nr = db.Column(db.Integer(), nullable=False)
+    erp_nr_parent = db.Column(db.Integer(), nullable=True)
+    image = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime(), nullable=True, default=datetime.datetime.now())
+    edited_at = db.Column(db.DateTime(), nullable=True, default=datetime.datetime.now())
+
+    # Relations
+    translations = db.relationship(
+        'BridgeCategoryTranslation',
+        backref='category',
+        lazy='dynamic',
+        cascade='all, delete-orphan')
+
+    marketplaces = db.relationship(
+        'BridgeMarketplaceEntity',
+        secondary=bridge_category_marketplace_association,
+        back_populates='categories',
+        cascade='all, delete'
+    )
+
+    def get_(self, language_code):
+        # Usage example:
+        # category = BridgeCategoryEntity.query.first()
+        # german_title = category.get_('DE_de').title
+        # english_description = category.get_('GB_en').description
+        translation = self.translations.filter_by(language=language_code).first()
+        return TranslationWrapper(translation)
+
+
+class BridgeCategoryTranslation(db.Model):
+    __tablename__ = 'bridge_category_translation'
+
+    id = db.Column(db.Integer(), primary_key=True, nullable=False, autoincrement=True)
+    language = db.Column(db.String(5), nullable=False)  # language format: 'DE_de', 'GB_en', etc.
+    name = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.Text(), nullable=True)
+    description_short = db.Column(db.Text(), nullable=True)
+
+    # Relations
+    category_id = db.Column(db.Integer(), db.ForeignKey('bridge_category_entity.id', ondelete='CASCADE'), nullable=False)
+
