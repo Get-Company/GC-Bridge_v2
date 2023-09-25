9630b0acdf95012b5d6c685319e4bb06a6e55049
contact@get-company.com <contact@get-company.com>
2023-09-25 16:14:48 +0200

    Added new Imports: Bridge Entities

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 from config import DBConfig
 
-
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -15,17 +16,31 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(DBConfig)
     db.init_app(app)
+    migrate.init_app(app, db)
 
     """ All the imports go here """
+# Entities
+    # ERP Entities are not necessary
 
-    # Controller
-    from .modules.ERP.controller.ERPKategorieController import ERPKategorieController
+    # Bridge
+    from .modules.Bridge.entities.BridgeCategoryEntity import BridgeCategoryEntity, BridgeCategoryTranslation, bridge_category_marketplace_association
+    from .modules.Bridge.entities.BridgeMarketplaceEntity import BridgeMarketplaceEntity
 
-    # Views
-    from .view.dataset_view import dataset_view
-    app.register_blueprint(dataset_view, url_prefix="/erp")
+# Controller
+    # ERP Controller
+    from .modules.ERP.controller.ERPArtikelKategorienController import ERPArtikelKategorienController
+    from .modules.ERP.controller.ERPArtikelController import ERPArtikelController
+    from .modules.ERP.controller.ERPAdressenController import ERPAdressenController
 
+    # Bridge Controller
+# Views
+    # ERP Views
+    from .view.dashboard_view import dashboard_view
+    app.register_blueprint(dashboard_view, url_prefix="/")
+
+# DB create_all to ensure all db tables are there
     with app.app_context():
         db.create_all()
 
+# Return the App Object
     return app
