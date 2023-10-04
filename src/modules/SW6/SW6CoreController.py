023085d579aaa82d912876becf55397c6ecaa620
contact@get-company.com <contact@get-company.com>
2023-10-04 15:01:53 +0200

    created mapping methods

--- src/modules/SW6/SW6CoreController.py
+++ src/modules/SW6/SW6CoreController.py
@@ -1,4 +1,5 @@
 from ..ModulesCoreController import ModulesCoreController
+from ..Bridge.entities.BridgeAbstractEntity import BridgeAbstractEntity
 from abc import abstractmethod
 
 
@@ -12,26 +13,28 @@ class SW6CoreController(ModulesCoreController):
     These Methods must bes overwritten. If there is no use of it simply do a pass!
     """
 
-    @abstractmethod
     def sync_all_to_bridge(self):
         pass
 
-    @abstractmethod
     def sync_all_from_bridge(self):
         pass
 
-    @abstractmethod
     def sync_one_to_bridge(self):
         pass
 
-    @abstractmethod
     def sync_one_from_bridge(self):
         pass
 
-    @abstractmethod
     def sync_changed_to_bridge(self):
         pass
 
-    @abstractmethod
     def sync_changed_from_bridge(self):
+        pass
+
+    @abstractmethod
+    def map_sw6_to_bridge(self):
+        pass
+
+    @abstractmethod
+    def map_bridge_to_sw6(self, bridge_ntt:BridgeAbstractEntity):
         pass
