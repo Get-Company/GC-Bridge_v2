1afe76221bff41ffe7cab0eaf1f07a18dcac6473
contact@get-company.com <contact@get-company.com>
2023-10-10 08:45:08 +0200

    Init

---
+++ src/modules/Bridge/Controller/BridgeAbstractController.py
@@ -0,0 +1,8 @@
+from ..BridgeCoreController import BridgeCoreController
+
+class BridgeAbstractController(BridgeCoreController):
+    def __init__(self):
+        super().__init__()
+
+    def delete_all(self):
+        pass
