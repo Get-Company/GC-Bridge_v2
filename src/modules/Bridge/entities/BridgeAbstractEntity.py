ec90cfa5ec19484bc2924a9bdd5b484ffb3e4411
contact@get-company.com <contact@get-company.com>
2023-12-19 09:02:53 +0100

    Added update method which should be implemented in the children

--- src/modules/Bridge/entities/BridgeAbstractEntity.py
+++ src/modules/Bridge/entities/BridgeAbstractEntity.py
@@ -4,3 +4,6 @@ from ..BridgeCoreController import BridgeCoreController
 class BridgeAbstractEntity(BridgeCoreController):
     def __init__(self):
         super().__init__()
+
+    def update(self, bridge_entity_new):
+        pass
