d3801d1ded9a215b01cd1256dea5ac14b89de597
contact@get-company.com <contact@get-company.com>
2023-12-20 07:52:42 +0100

    Some tweaks

--- src/modules/Bridge/controller/BridgeCategoryController.py
+++ src/modules/Bridge/controller/BridgeCategoryController.py
@@ -1,5 +1,6 @@
 from .BridgeAbstractController import BridgeAbstractController
 from ..entities.BridgeCategoryEntity import BridgeCategoryEntity
+import json
 
 
 class BridgeCategoryController(BridgeAbstractController):
@@ -10,7 +11,11 @@ class BridgeCategoryController(BridgeAbstractController):
     def get_entity(self):
         return BridgeCategoryEntity()
 
-
-
-
-
+    def get_main_category(self, tree_path):
+        tree_path_list = json.loads(tree_path)
+        main_category_id = tree_path_list[0]
+        main_category = self._bridge_entity.query.filter_by(erp_nr=main_category_id).one_or_none()
+        if main_category:
+            return main_category
+        else:
+            return None
