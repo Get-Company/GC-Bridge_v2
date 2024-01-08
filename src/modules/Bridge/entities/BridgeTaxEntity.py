36c35c5b4ef7d915af951bfbd98f5a7213739fb2
contact@get-company.com <contact@get-company.com>
2024-01-08 09:03:00 +0100

    Changed DB Field description to text without length

--- src/modules/Bridge/entities/BridgeTaxEntity.py
+++ src/modules/Bridge/entities/BridgeTaxEntity.py
@@ -7,7 +7,7 @@ class BridgeTaxEntity(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True, nullable=False, autoincrement=True)
     erp_nr = db.Column(db.Integer(), nullable=False, unique=True)
-    description = db.Column(db.Text(4294967295), nullable=True)
+    description = db.Column(db.Text(), nullable=True)
     key = db.Column(db.Float(), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=True, default=datetime.datetime.now())
     edited_at = db.Column(db.DateTime(), nullable=True, default=datetime.datetime.now())
