9f90587b45c194d039744ad7d9b12c47b71a8967
contact@get-company.com <contact@get-company.com>
2024-02-09 11:53:08 +0100

    Create vapp_dashboard.js for Vue app
    Added a new JavaScript file, vapp_dashboard.js, for managing Vue application actions. This file contains initial Vue instance setup including data properties and method placeholders. The main data properties are 'orders' and 'progress' which are set for managing orders information and process progress respectively.

---
+++ src/static/js/bridge/dashboard/vapp_dashboard.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    delimiters: ['${', '}'],
+    data() {
+        return {
+            orders: [],
+            progress: -1, // Init value for the progress bar
+        };
+    },
+    methods: {
+
+    },
+
+})
