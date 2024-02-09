2703aef78755a24d138e17c44cfac460dadd624c
contact@get-company.com <contact@get-company.com>
2024-02-09 11:11:53 +0100

    Add new BridgeOrderController module
    BridgeOrderController is introduced in the 'Bridge' module. This controller includes functions to get all orders and orders by given date range, enhancing the functionality and providing user with more flexibility to handle the orders.

---
+++ src/modules/Bridge/controller/BridgeOrderController.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from pprint import pprint
+
+from .BridgeAbstractController import BridgeAbstractController
+from ..entities.BridgeOrderEntity import BridgeOrderEntity
+
+
+class BridgeOrderController(BridgeAbstractController):
+    def __init__(self):
+        self._bridge_entity = BridgeOrderEntity()
+        super().__init__(bridge_entity=self._bridge_entity)
+
+    def get_all_orders(self):
+        orders = self.get_entity().query.all()
+        if orders:
+            print(f"Found {len(orders)} orders")
+            return orders
+        else:
+            print("No orders found")
+            return None
+
+    def get_orders_by_date(self, start_date=None, end_date=None):
+        # Implementieren Sie Ihre Logik hier, um die Daten zu filtern
+        # basierend auf start_date, end_date, oder beiden
+        # Beispiel:
+
+        # Format für Datum und Zeit
+        date_format = '%Y-%m-%dT%H:%M'
+
+        if start_date and not isinstance(start_date, datetime):
+            start_date = datetime.strptime(start_date, date_format)
+            start_date = start_date.replace(second=0)
+
+        if end_date and not isinstance(end_date, datetime):
+            end_date = datetime.strptime(end_date, date_format)
+            end_date = end_date.replace(second=0)
+
+        query = BridgeOrderEntity.query
+        if start_date:
+            query = query.filter(BridgeOrderEntity.purchase_date >= start_date)
+        if end_date:
+            query = query.filter(BridgeOrderEntity.purchase_date <= end_date)
+
+        return query.all(), start_date, end_date
