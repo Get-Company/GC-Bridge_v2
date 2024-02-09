1002f704df79bbaa871d8542de5a0f603be7e6b9
contact@get-company.com <contact@get-company.com>
2024-02-09 11:06:32 +0100

    Add get_id and set_id methods to BridgeAbstractEntity
    Implemented getter and setter methods for the id parameter within the BridgeAbstractEntity class. The set_id function also includes a validation check to ensure the id cannot be set to None or a negative value.

--- src/modules/Bridge/entities/BridgeAbstractEntity.py
+++ src/modules/Bridge/entities/BridgeAbstractEntity.py
@@ -7,3 +7,16 @@ class BridgeAbstractEntity(BridgeCoreController):
 
     def update(self, bridge_entity_new):
         pass
+
+    # Getter and Setter for id
+    def get_id(self):
+        return self.id
+
+    def set_id(self, id):
+        pass
+        try:
+            if id is None or id < 0:
+                raise ValueError("ID cannot be None or negative.")
+            self.id = id
+        except ValueError as e:
+            print(f"Error setting id: {e}")
