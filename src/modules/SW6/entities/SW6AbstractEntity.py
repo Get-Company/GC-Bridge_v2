b3727eac6dcbed04c7eac1ab1466addb787bcf90
contact@get-company.com <contact@get-company.com>
2024-02-09 11:46:16 +0100

    Refactor SW6AbstractEntity with better API methods
    The code modifies SW6AbstractEntity by introducing new methods that interact with the API endpoints for getting, patching, and searching data. It also refactors the session handling for optimized database performance and enhances functionality such as error logging and entity mapping.

--- src/modules/SW6/entities/SW6AbstractEntity.py
+++ src/modules/SW6/entities/SW6AbstractEntity.py
@@ -1,7 +1,9 @@
 from pprint import pprint
+from config import SW6Config
 
 from ..SW6CoreController import SW6CoreController
 from lib_shopware6_api_base import Shopware6AdminAPIClientBase, Criteria, EqualsFilter
+
 from config import ConfShopware6ApiBase as SW6Config
 
 
@@ -12,45 +14,36 @@ class SW6AbstractEntity(SW6CoreController):
         # SW6Config is my config with the same Attributes
         self.sw6_client = Shopware6AdminAPIClientBase(config=SW6Config)
         self._endpoint_name = endpoint_name
+        self._criteria = Criteria()
+        self.config_sw6 = SW6Config()
 
-    def request(self):
-        id = 'e2f18bf14dd54320952d73a0af868dde'
-        category = self.sw6_client.request_get(f"/category/{id}")
-        pprint(category)
+    def get_api_(self, id):
+        result = self.sw6_client.request_get(f"/{self._endpoint_name}/{id}")
+        return result
 
-    def get_(self, id):
-        endpoint = self.sw6_client.request_get(f"/{self._endpoint_name}/{id}")
-        return endpoint
+    def get_api_list(self):
+        result = self.sw6_client.request_get(f"/{self._endpoint_name}")
+        return result
 
-    def search_by_(self, index_field=None, search_value=None):
+    def patch_customer_number_by_customer_id(self, customer_id, new_customer_nr):
+        payload = {'customerNumber': new_customer_nr}
+        result = self.sw6_client.request_patch(f"/{self._endpoint_name}/{customer_id}", payload=payload)
+        return result
+
+    def search_api_by_(self, index_field=None, search_value=None):
         payload = Criteria()
-        payload.filter.append(EqualsFilter(index_field, search_value))
+        payload.filter.append(EqualsFilter(field=index_field, value=search_value))
         endpoint = self.sw6_client.request_post(f"/search/{self._endpoint_name}", payload=payload)
         return endpoint
 
+    def search_api_ids_by_(self, index_field=None, search_value=None):
+        payload = Criteria()
+        payload.filter.append(EqualsFilter(field=index_field, value=search_value))
+        endpoint = self.sw6_client.request_post(f"search-ids/{self._endpoint_name}", payload=payload)
+        return endpoint
 
-
-
-
-
-    """     
-    Abstract Methods, defined in ModulesCoreController
-    These Methods must be overwritten. If there is no use of it simply do a pass!
     """
-    def sync_all_to_bridge(self):
-        pass
-
-    def sync_all_from_bridge(self):
-        pass
-
-    def sync_one_to_bridge(self):
-        pass
-
-    def sync_one_from_bridge(self):
-        pass
-
-    def sync_changed_to_bridge(self):
-        pass
-
-    def sync_changed_from_bridge(self):
-        pass
+    Getter and Setter
+    """
+    def get_id(self, data):
+        return data["id"]
