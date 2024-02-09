132a1cc0ef915274c0b938571e2abbfe5ce42bff
contact@get-company.com <contact@get-company.com>
2024-02-09 11:46:33 +0100

    Refactor SW6CoreController and update method usage
    The SW6CoreController was refined; unused imports were removed and method signatures were updated to match the actual usage. In addition, the abstract methods 'map_sw6_to_bridge' and 'map_bridge_to_sw6' were removed as they're not used in the current context.

--- src/modules/SW6/SW6CoreController.py
+++ src/modules/SW6/SW6CoreController.py
@@ -1,5 +1,4 @@
 from ..ModulesCoreController import ModulesCoreController
-from ..Bridge.entities.BridgeAbstractEntity import BridgeAbstractEntity
 from abc import abstractmethod
 
 
@@ -10,9 +9,8 @@ class SW6CoreController(ModulesCoreController):
 
     """     
     Abstract Methods, defined in ModulesCoreController
-    These Methods must bes overwritten. If there is no use of it simply do a pass!
+    These Methods must be overwritten. If there is no use of it simply do a pass!
     """
-
     def sync_all_to_bridge(self):
         pass
 
@@ -22,19 +20,11 @@ class SW6CoreController(ModulesCoreController):
     def sync_one_to_bridge(self):
         pass
 
-    def sync_one_from_bridge(self):
+    def sync_one_from_bridge(self, bridge_id):
         pass
 
     def sync_changed_to_bridge(self):
         pass
 
     def sync_changed_from_bridge(self):
-        pass
-
-    @abstractmethod
-    def map_sw6_to_bridge(self):
-        pass
-
-    @abstractmethod
-    def map_bridge_to_sw6(self, bridge_ntt:BridgeAbstractEntity):
         pass
