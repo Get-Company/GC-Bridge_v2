cab683c88e826a9cde6826b8cadad88fe4395cf8
contact@get-company.com <contact@get-company.com>
2024-03-07 08:32:19 +0100

    Refactor price calculation in SW6OrderDetailsEntity
    Refactored the price calculation methods in SW6OrderDetailsEntity.py. Two methods 'get_unit_price' and 'get_total_price' were introduced to handle price calculation with tax considerations. An additional piece of logical implementation was added for retrieving tax data. These changes aim to improve price calculation by more accurately reflecting tax rules.

--- src/modules/SW6/entities/SW6OrderDetailsEntity.py
+++ src/modules/SW6/entities/SW6OrderDetailsEntity.py
@@ -18,7 +18,8 @@ class SW6OrderDetailsEntity(SW6AbstractEntity):
                 erp_nr=self.get_erp_nr(sw6_json_data),
                 api_id=self.get_api_id(sw6_json_data),
                 api_order_id=self.get_api_order_id(sw6_json_data),
-                price=self.get_price(sw6_json_data),
+                unit_price=self.get_unit_price(sw6_json_data),
+                total_price=self.get_total_price(sw6_json_data),
                 quantity=self.get_quantity(sw6_json_data),
                 name=self.get_name(sw6_json_data),
                 tax=self.get_tax(sw6_json_data)
@@ -41,14 +42,181 @@ class SW6OrderDetailsEntity(SW6AbstractEntity):
             self.logger.error(f"Error getting api_id (ID LineItem): {e}")
             return None
 
-    def get_price(self, sw6_json_data):
+    def get_unit_price(self, sw6_json_data):
+        """
+        Calculate the net unit price of a product.
+
+        This function calculates the net unit price based on the calculated taxes
+        and tax rules if both are present in the product object.
+
+        If the 'calculatedTaxes' field is not present in the product object,
+        it will return the unit price directly.
+
+        :param product: A JSON object which represents a product and potentially has the following fields:
+                        - calculatedTaxes: A boolean field specifying whether the prices are calculated with taxes.
+                        - taxRules: A list wherein the first element's 'taxRate' field is used to calculate net prices.
+                        - unitPrice: The unit price of the product.
+        :return: The net unit price if the required fields are present, otherwise, the unit price.
+        B2B Customer Data:
+        sw6_json_data = {
+            'price': {
+                'apiAlias': 'calculated_price',
+                'calculatedTaxes': [],
+                'extensions': [],
+                'listPrice': {
+                    'apiAlias': 'cart_list_price',
+                    'discount': -5.976890756303,
+                    'extensions': [],
+                    'percentage': 8.89,
+                    'price': 67.226890756303
+                },
+                'quantity': 100,
+                'referencePrice': None,
+                'regulationPrice': None,
+                'taxRules': [],
+                'totalPrice': 6125,
+                'unitPrice': 61.25
+            }
+        }
+        B2C Customer Data:
+        sw6_json_data = {
+            'price': {
+                'apiAlias': 'calculated_price',
+                'calculatedTaxes': [{
+                    'apiAlias': 'cart_tax_calculated',
+                    'extensions': [],
+                    'price': 7551,
+                    'tax': 1205.62,
+                    'taxRate': 19
+                }],
+                'extensions': [],
+                'listPrice': {
+                    'apiAlias': 'cart_list_price',
+                    'discount': -4.49,
+                    'extensions': [],
+                    'percentage': 5.61,
+                    'price': 80
+                },
+                'quantity': 100,
+                'referencePrice': None,
+                'regulationPrice': None,
+                'taxRules': [{
+                    'apiAlias': 'cart_tax_rule',
+                    'extensions': [],
+                    'percentage': 100,
+                    'taxRate': 19
+                }],
+                'totalPrice': 7551,
+                'unitPrice': 75.51
+            }
+        }
+        """
         try:
-            # Just return the net price - is it really net?
-            calculated_price = sw6_json_data['price']['calculatedTaxes'][0]['price']
-            calculated_tax = sw6_json_data['price']['calculatedTaxes'][0]['tax']
-            net_price = calculated_price - calculated_tax
-            return net_price
+            # Check if 'calculatedTaxes' and 'taxRules' fields are in the Json data and are not empty
+            if 'calculatedTaxes' in sw6_json_data and sw6_json_data['taxRules']:
+                # Get the tax rate from the first element of the 'taxRules' field
+                tax_rate = sw6_json_data['taxRules'][0]['taxRate']
+                brutto_unit_price = sw6_json_data['unitPrice']
+                # Calculate the net unit price based on the tax rate
+                netto_unit_price = brutto_unit_price / (1 + tax_rate / 100)
+                return round(netto_unit_price, 2)
+            else:
+                # Return the brutto unit price directly if 'calculatedTaxes' field is not present in the product object
+                return sw6_json_data['unitPrice']
         except Exception as e:
+            # Log the error and return None if there was an error during calculation
+            self.logger.error(f"Error calculating price: {e}")
+            return None
+
+    def get_total_price(self, sw6_json_data):
+        """
+        Calculate the net total price of a product.
+
+        This function calculates the net total price based on the calculated taxes,
+        tax rules, unit price and quantity if all are present in the product object.
+
+        If the 'calculatedTaxes' field is not present in the product object,
+        it will return the total price directly.
+
+        :param product: A JSON object which represents a product and has the following fields:
+                        - calculatedTaxes: A boolean field specifying whether the prices are calculated with taxes.
+                        - taxRules: A list wherein the first element's 'taxRate' field is used to calculate net prices.
+                        - unitPrice: The unit price of the product.
+                        - quantity: The quantity of the product.
+                        - totalPrice: The total price of the product.
+        :return: The net total price if the required fields are present, otherwise, the total price.
+
+        B2B Customer Data:
+        sw6_json_data = {
+            'price': {
+                'apiAlias': 'calculated_price',
+                'calculatedTaxes': [],
+                'extensions': [],
+                'listPrice': {
+                    'apiAlias': 'cart_list_price',
+                    'discount': -5.976890756303,
+                    'extensions': [],
+                    'percentage': 8.89,
+                    'price': 67.226890756303
+                },
+                'quantity': 100,
+                'referencePrice': None,
+                'regulationPrice': None,
+                'taxRules': [],
+                'totalPrice': 6125,
+                'unitPrice': 61.25
+            }
+        }
+        B2C Customer Data:
+        sw6_json_data = {
+            'price': {
+                'apiAlias': 'calculated_price',
+                'calculatedTaxes': [{
+                    'apiAlias': 'cart_tax_calculated',
+                    'extensions': [],
+                    'price': 7551,
+                    'tax': 1205.62,
+                    'taxRate': 19
+                }],
+                'extensions': [],
+                'listPrice': {
+                    'apiAlias': 'cart_list_price',
+                    'discount': -4.49,
+                    'extensions': [],
+                    'percentage': 5.61,
+                    'price': 80
+                },
+                'quantity': 100,
+                'referencePrice': None,
+                'regulationPrice': None,
+                'taxRules': [{
+                    'apiAlias': 'cart_tax_rule',
+                    'extensions': [],
+                    'percentage': 100,
+                    'taxRate': 19
+                }],
+                'totalPrice': 7551,
+                'unitPrice': 75.51
+            }
+        }
+        """
+
+        try:
+            # Check if there is 'calculatedTaxes' field in the 'price' field
+            # and if it's not empty
+            if 'calculatedTaxes' in sw6_json_data and sw6_json_data['taxRules']:
+                # Calculate the net unit price
+                netto_unit_price = self.get_unit_price(sw6_json_data=sw6_json_data)
+                # Get the quantity from the product object
+                quantity = sw6_json_data['quantity']
+                # Calculate the net total price based on the net unit price and quantity
+                netto_total_price = netto_unit_price * quantity
+                return round(netto_total_price, 2)
+            else:
+                # Return the bruto total price directly if 'calculatedTaxes' is not present in the product object
+                return sw6_json_data['totalPrice']
+        except Exception as e:
+            # Log the error and return None if there was an error during calculation
             self.logger.error(f"Error calculating price: {e}")
             return None
 
@@ -67,10 +235,43 @@ class SW6OrderDetailsEntity(SW6AbstractEntity):
             return None
 
     def get_tax(self, sw6_json_data):
+        """
+        This function retrieves the tax amount based on the order line item data from Shopware 6.
+        If the tax data is not found or is empty, the function will return None.
+
+        :param sw6_json_data: dict, Order line item data from Shopware 6
+        :return: float, the tax amount. None if the tax data was not found or an error occurred.
+
+        #Example usage:
+
+        sw6_json_data = {
+            'price': {
+                'calculatedTaxes': [{
+                    'tax': 1205.62
+                    # other fields...
+                }]
+                # other fields...
+            }
+            # other fields...
+        }
+
+        tax_amount = get_tax(sw6_json_data)
+        print(tax_amount)  # prints: 1205.62
+        """
         try:
-            return sw6_json_data['price']['calculatedTaxes'][0]['taxRate']
+            # Check if there is 'calculatedTaxes' field in the 'price' field
+            # and if it's not empty
+            if 'calculatedTaxes' in sw6_json_data['price'] and sw6_json_data['price']['calculatedTaxes']:
+                # If it exists and not empty, get the first item's 'tax'
+                tax_amount = sw6_json_data['price']['calculatedTaxes'][0]['tax']
+            else:
+                # If 'calculatedTaxes' does not exist or is empty,
+                # There is no tax amount, returns None
+                tax_amount = None
+            return tax_amount
         except Exception as e:
-            self.logger.error(f"Error getting tax rate: {e}")
+            # Log the error and return None if there was an error
+            self.logger.error(f"Error getting tax amount: {e}")
             return None
 
     def get_api_order_id(self, sw6_json_data):
@@ -79,7 +280,3 @@ class SW6OrderDetailsEntity(SW6AbstractEntity):
         except Exception as e:
             self.logger.error(f"Error getting order ID: {e}")
             return None
-
-
-
-
