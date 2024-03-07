463fc920dbd35a23ac3150dafaceadba434fcaa3
contact@get-company.com <contact@get-company.com>
2024-03-07 08:31:04 +0100

    Incorporate sourcemaps in gulp build process
    The gulpfile.js has been modified to utilize sourcemaps during JS build creation. The sourcemaps are initialized at the start of the task, with write operation carried out after combining all JS files. This inclusion will aid in debugging by mapping code within a compressed production file back to its original core.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
+// Run gulp by gulp build-js
+
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 // const browserify = require('gulp-browserify');
 
 // Definieren der Quellpfade für Bootstrap-JavaScript-Dateien
@@ -7,8 +10,10 @@ const bootstrapJsPath = 'node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 gulp.task('build-js', function(){
    return gulp.src(bootstrapJsPath)  // Pfad zu Bootstrap-JavaScript-Dateien
+       .pipe(sourcemaps.init())  // Initialisiert die Erstellung der Source-Map
        .pipe(concat('bootstrap.bundle.js'))  // Kombinieren in eine Datei namens bootstrap.js
        // .pipe(browserify())  // Optional: Verwenden Sie Browserify für Modulbündelung, wenn benötigt
+       .pipe(sourcemaps.write('.'))  // Schreibt die Source-Map in dieselbe Quelle wie das Zielverzeichnis
        .pipe(gulp.dest('src/static/js'));  // Zielverzeichnis
 });
 
