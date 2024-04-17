8f92d5ce8df235b84847b6ffe66eb5853f167e38
contact@get-company.com <contact@get-company.com>
2024-04-17 09:56:16 +0200

    Remove product-related routes from BridgeViews
    The routes related to products and their functionalities have been eliminated from the BridgeViews.py. This includes the routes for displaying product listings, single product details, and retrieving products by ERP number.
    
    Had Darth Vader been more diligent about reducing redundancy in the Death Star's architectural blueprint (like this code cleanup), perhaps Luke Skywalker wouldn't have found that pesky exhaust port leading right to the main reactor. Rookie programming mistake, Dark Side.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/view/BridgeViews.py
+++ src/modules/Bridge/view/BridgeViews.py
@@ -24,26 +24,6 @@ def bridge_categories():
     return render_template('bridge/category/categories.html', categories=categories)
 
 
-@BridgeViews.route('/products', endpoint='products')
-def bridge_products():
-    page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 10, type=int)
-    search = request.args.get('search', '', type=str)
-    sort_by = request.args.get('sort_by', 'id')  # default column to sort by
-    sort_order = request.args.get('sort_order', 'asc', type=str)  # default sort order
-
-    query = BridgeProductEntity.query
-
-    if search:
-        # for example if your entity has 'name' and 'description' exposed
-        query = query.filter(
-            BridgeProductEntity.erp_nr.contains(search)
-        )
-
-    products = query.paginate(page=page, per_page=per_page, error_out=False)
-    return render_template('bridge/product/products.html', products=products, per_page=per_page)
-
-
 @BridgeViews.route('/inventur', endpoint='inventur')
 def bridge_products_inventur():
     page = request.args.get('page', 1, type=int)
@@ -62,34 +42,6 @@ def bridge_products_inventur():
 
     products = query.paginate(page=page, per_page=per_page, error_out=False)
     return render_template('bridge/product/inventur.html', products=products, per_page=per_page)
-
-
-@BridgeViews.route('/product/<id>', endpoint='product')
-def bridge_product(id):
-    product = BridgeProductEntity.query.get(id)
-
-    if product:
-
-        return render_template('bridge/product/product.html',
-                               product=product,
-                               assets_img_path=GCBridgeConfig.ASSETS_PATH + "/" + GCBridgeConfig.IMG_PATH
-                               )
-    else:
-        abort(404)
-
-
-@BridgeViews.route('/product/erp_nr/<erp_nr>', endpoint='product_by_erp_nr')
-def bridge_product_by_erp_nr(erp_nr):
-    product = BridgeProductEntity.query.filter_by(erp_nr=erp_nr).one_or_none()
-
-    if product:
-
-        return render_template('bridge/product/product.html',
-                               product=product,
-                               assets_img_path=GCBridgeConfig.ASSETS_PATH + "/" + GCBridgeConfig.IMG_PATH
-                               )
-    else:
-        abort(404)
 
 
 @BridgeViews.route('/marketplaces', endpoint='marketplaces')
