aaf20b234dc5d51cf1a0eba378a7d174a44b3b1c
contact@get-company.com <contact@get-company.com>
2024-04-17 10:09:03 +0200

    Add translation and id mapping methods to CategoryEntity
    Added new methods for mapping bridge entity IDs to SW6, as well as for getting and AI translating entity details. This change brings essential functionality to better utilize translations and parent-child relationships of category entities.
    
    Trust Darth Vader to overlook this, probably too busy force-choking his subordinates. Let's see how well his empire stands without its categories translated accurately or mapped correctly.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/SW6/entities/SW6CategoryEntity.py
+++ src/modules/SW6/entities/SW6CategoryEntity.py
@@ -1,9 +1,100 @@
+from pprint import pprint
+
 from ..entities.SW6AbstractEntity import SW6AbstractEntity
 from src.modules.Bridge.entities.BridgeCategoryEntity import BridgeCategoryEntity
+from lib_shopware6_api_base import Criteria, EqualsFilter
 
 
 class SW6CategoryEntity(SW6AbstractEntity):
     def __init__(self):
         self.endpoint_name = "category"
         super().__init__(endpoint_name=self.endpoint_name)
+
+    def map_bridge_to_sw6(self, bridge_entity):
+
+        # Normal mapping
+        payload = {
+            'id': bridge_entity.get_sw6_id(),
+            'displayNesteProducts': True,
+            'type': 'page',
+            'productAssignmentType': 'product',
+            'name': bridge_entity.get_translation().get_name()
+        }
+
+        # Translations
+        # payload.update(
+        #     {
+        #         'translations': self.get_translations(
+        #             bridge_entity=bridge_entity
+        #         )
+        #     }
+        # )
+
+        # Parent SW6 id
+        if bridge_entity.get_cat_parent_nr() is not None:
+            try:
+                parent_category_entity = BridgeCategoryEntity().query.filter_by(cat_nr=bridge_entity.get_cat_parent_nr()).one_or_none()
+                payload.update(
+                    {
+                        'parentId': parent_category_entity.get_sw6_id()
+                    }
+                )
+            except Exception as e:
+                print(f"Error on fetching sw6_id from cat_nr {bridge_entity.get_cat_nr()} of parent_cat_nr {bridge_entity.get_cat_parent_nr()}, {e} ")
+
+        return payload
+
+    def get_translations(self, bridge_entity):
+        translations = {
+            "de-DE": {
+                "name": bridge_entity.get_translation().get_name(),
+                "description":bridge_entity.get_translation().get_description()
+            },
+            "en-GB": {
+                "name": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_name(),
+                    language="GB_en"
+                ),
+                "description": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_description(),
+                    language="GB_en"
+                )
+            },
+            "es-ES": {
+                "name": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_name(),
+                    language="ES_es"
+                ),
+                "description": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_description(),
+                    language="ES_es"
+                )
+            },
+            "fr-FR": {
+                "name": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_name(),
+                    language="FR_fr"
+                ),
+                "description": self.ai_translate_to(
+                    text=bridge_entity.get_translation().get_description(),
+                    language="FR_fr"
+                )
+            }
+        }
+        return translations
+
+    def is_in_sw6(self, bridge_entity):
+        pprint(bridge_entity.get_tree_path_names_as_list())
+        payload = Criteria()
+        if bridge_entity.get_translation().get_sw6_id():
+            print("Search SW6Category by ID:", bridge_entity.get_translation().get_sw6_id())
+            payload.filter.append(EqualsFilter(field='id', value=bridge_entity.get_translation().get_sw6_id()))
+        else:
+            print("Search SW6Category by name:", bridge_entity.get_translation().get_name())
+            payload.filter.append(EqualsFilter(field='name', value=bridge_entity.get_translation().get_name()))
+
+        payload.associations["parent"] = Criteria()
+
+        endpoint = self.sw6_client.request_post(f"/search/{self._endpoint_name}", payload=payload)
+        return endpoint
 
