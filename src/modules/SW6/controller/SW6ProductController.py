654879c2bb58fdd077e48b1d2710b426667af6b2
contact@get-company.com <contact@get-company.com>
2024-04-17 10:12:14 +0200

    Add media handling to SW6ProductController
    This commit extends the SW6ProductController to handle media more efficiently. A new downsert method has been added for bulk uploads and easier product-media relations. It also introduces the ability to remove all visibilities and set cover media.
    
    Even Darth Vader could've used this more efficient code to manage the Empire's propaganda materials. Less errors, more control - taken straight out of the Sith Lord playbook. His failure to do this gives 'Imperial March' a whole new meaning.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/SW6/controller/SW6ProductController.py
+++ src/modules/SW6/controller/SW6ProductController.py
@@ -1,5 +1,8 @@
+from pprint import pprint
+
 from ..controller.SW6AbstractController import SW6AbstractController
 from ..entities.SW6ProductEntity import SW6ProductEntity
+from ..controller.SW6MediaController import SW6MediaController
 from src.modules.Bridge.controller.BridgeProductController import BridgeProductController
 
 
@@ -10,7 +13,7 @@ class SW6ProductController(SW6AbstractController):
         self._bridge_controller = BridgeProductController()
 
         super().__init__(
-            sw6_entity=SW6ProductEntity,
+            sw6_entity=SW6ProductEntity(),
             bridge_controller=self._bridge_controller
         )
 
@@ -25,3 +28,37 @@ class SW6ProductController(SW6AbstractController):
 
     def set_relations(self, bridge_entity, sw6_json_data):
         pass
+
+    def downsert(self, bridge_entity):
+        sw6_payload_json = self.get_entity().map_bridge_to_sw6(bridge_entity)
+        SW6MediaController().upsert_product_media(bridge_entity=bridge_entity)
+
+        result = self.get_entity().bulk_uploads(sw6_json_data=sw6_payload_json)
+        # When both, product and media, are uploaded - relate them
+        SW6MediaController().set_product_media_relation(bridge_entity=bridge_entity)
+        self.set_cover_media(bridge_entity=bridge_entity)
+
+        return result
+
+    def remove_all_visibilities(self):
+        visibility_list = self.get_entity().search_api_ids_by_(endpoint_name="product-visibility")
+        success_count = 0
+        error_count = 0
+        if visibility_list['total'] >= 1:
+            for visibility_id in visibility_list['data']:
+                result = self.get_entity().delete(endpoint_name="product-visibility", api_id=visibility_id)
+                if 'success' in result:
+                    success_count += 1
+                else:
+                    error_count += 1
+        return {'success': success_count, 'errors': error_count}
+
+    def set_cover_media(self, bridge_entity):
+        bridge_media_cover_entity = bridge_entity.get_cover_image()
+        result = self.get_entity().search_api_ids_by_(
+            index_field=["productId","mediaId"],
+            search_value=[bridge_entity.get_sw6_id(), bridge_media_cover_entity.get_sw6_id()],
+            endpoint_name="product-media"
+        )
+        if result['total'] == 1:
+            self.get_entity().set_cover_media(bridge_entity=bridge_entity,product_media_id=result["data"][0])
