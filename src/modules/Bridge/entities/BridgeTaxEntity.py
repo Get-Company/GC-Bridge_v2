c9891ceb30f72aa52f2f4508b28a792d94edfb56
contact@get-company.com <contact@get-company.com>
2024-04-17 09:54:24 +0200

    Add getter and setter methods to BridgeTaxEntity
    Added getter and setter methods for attributes in the BridgeTaxEntity. This includes handling potential exceptions which ensures error handling and increases stability in the program.
    
    It seems like something Darth Vader should have done at the Death Star; had error handling been prioritized, maybe that one fatal weak point would have been found and fixed. Alas, a lack of similar foresight led to a spectacular failure.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/entities/BridgeTaxEntity.py
+++ src/modules/Bridge/entities/BridgeTaxEntity.py
@@ -15,6 +15,57 @@ class BridgeTaxEntity(db.Model):
     # Relations
     products = db.relationship('BridgeProductEntity', backref='tax', lazy=True)
 
+    def get_erp_nr(self):
+        try:
+            return self.erp_nr
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def set_erp_nr(self, erp_nr):
+        try:
+            self.erp_nr = erp_nr
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def get_description(self):
+        try:
+            return self.description
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def set_description(self, description):
+        try:
+            self.description = description
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def get_key(self):
+        try:
+            return self.key
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def set_key(self, key):
+        try:
+            self.key = key
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def get_key_to_calculate(self):
+        return ((self.key / 100) + 1)
+
+    def get_edited_at(self):
+        try:
+            return self.edited_at
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def set_edited_at(self, edited_at):
+        try:
+            self.edited_at = edited_at
+        except Exception as e:
+            print(f"Error: {e}")
+
     def __repr__(self):
         return f'Bridge Tax Entity: {self.description} ID: {self.id}'
 
