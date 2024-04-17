7526533bc8176434fde3c36e907c41bf2d23cb9c
contact@get-company.com <contact@get-company.com>
2024-04-17 10:08:26 +0200

    Implement SW6CategoryController class
    The SW6CategoryController class is implemented, inheriting from SW6AbstractController. It includes methods for database checks and setting relations. An instance of BridgeCategoryController is used for fetching data from the bridge and upsert operations are overridden for specific ordering.
    
    Darth Vader should have checked his programming for rebels just as thoroughly. If he had, the Death Star project management system might not have suffered from "unexpected" flaws and met with such a 'dramatic' end.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

---
+++ src/modules/SW6/controller/SW6CategoryController.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+
+from sqlalchemy import asc
+
+from ..controller.SW6AbstractController import SW6AbstractController
+from ..entities.SW6CategoryEntity import SW6CategoryEntity
+from src.modules.Bridge.controller.BridgeCategoryController import BridgeCategoryController
+from src.modules.Bridge.entities.BridgeCategoryEntity import BridgeCategoryEntity
+
+class SW6CategoryController(SW6AbstractController):
+
+    def __init__(self):
+
+        self._bridge_controller = BridgeCategoryController()
+
+        super().__init__(
+            sw6_entity=SW6CategoryEntity(),
+            bridge_controller=self._bridge_controller
+        )
+
+    def is_in_db(self, bridge_entity, sw6_json_data):
+        bridge_product_entity_in_db = self._bridge_controller.get_entity().query.filter_by(erp_nr=bridge_entity.erp_nr).one_or_nne()
+        if bridge_product_entity_in_db:
+            self.logger.info(f"Entity {bridge_entity.api_id} found in the db!")
+            return bridge_product_entity_in_db
+        else:
+            self.logger.info(f"No Entity {bridge_entity.api_id} found in the db!")
+            return None
+
+    def set_relations(self, bridge_entity, sw6_json_data):
+        pass
+
+    def sync_all_from_bridge(self):
+        # Overwrite, since we ned a special order (cat_nr)
+        categories = BridgeCategoryEntity.query.order_by(BridgeCategoryEntity.cat_nr.asc()).all()
+        # pprint(categories)
+        for category in categories:
+            print(f"Upsert Category: {category.get_translation().get_name()} id. {category.get_cat_nr()}")
+            self.sync_one_from_bridge(bridge_entity=category)
+
+
+
