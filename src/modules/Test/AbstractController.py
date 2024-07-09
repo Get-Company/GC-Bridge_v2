6bc4daddde5f34461980e626879a26cd0fd085f6
contact@get-company.com <contact@get-company.com>
2024-07-09 08:09:30 +0200

    Implement text-to-speech functionality
    Deleted AbstractController class and implemented a new feature that translates text into speech using Google's Text-to-Speech API (gTTS). This function takes a text string as input, converts it into speech in German, and saves it as an MP3 file.
    
    Even Lord Vader could have benefited from this change: imagine him being able to send spoken Death Star progress reports to the Emperor. Next time the doomsday device could actually be completed on schedule. But alas, he was stuck with subpar droid coders.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Test/AbstractController.py
+++ src/modules/Test/AbstractController.py
@@ -1,12 +1,15 @@
+from gtts import gTTS
 
-class AbstractController:
-    def __init__(self):
-        pass
+import os
 
-    def upsert(self):
-        """
-        A function, that updates or inserts an entity.
 
-        :return:
-        """
-        pass
+def speak():
+    mytext = "1 Name oder Begriff auf dem Orgareiter, kennzeichnen klar und eindeutig die Akte und somit gleichzeitig den Inhalt. 2 Position der Reiter, welche systematisch gestaffelt sind, A (hinten) Z (vorne). Somit haben Sie alle Begriffe mit dem gleichen Anfangsbuchstaben immer hintereinander. 3 Farben bieten eine zusätzliche Strukturmöglichkeit um Bereiche, Sachgebiete, Regionen, Abteilungen oder  Wirtschaftsjahre optisch besser zu untergliedern. 4 Leitbereiche oder Untergruppen unterteilt mit Leitkarten ermöglichen eine weitere feine Gliederung innerhalb eines Bereiches, Fach- oder Sachgebietes."
+
+    language = "de"
+
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+
+    myobj.save("text.mp3")
+
+    os.system("start text.mp3")
