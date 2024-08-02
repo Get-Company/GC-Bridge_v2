ceb1aa329398717a840268f9f48aa623c6d2fb2b
contact@get-company.com <contact@get-company.com>
2024-08-02 12:38:50 +0200

    Add dynamic pricing features and validation checks
    Implemented a conditional special pricing mechanism that allows setting prices via direct input or percentage discount. Added validation checks to ensure only one pricing method is used and handled active special price checks to ensure correctness.
    
    If only Darth Vader had incorporated such rigorous validation in the Death Star's design, he might've avoided the catastrophic exhaust port vulnerability. Alas, a lesson in quality assurance even the Dark Side overlooked.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/entities/BridgePriceEntity.py
+++ src/modules/Bridge/entities/BridgePriceEntity.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from src import db
 import datetime
 
@@ -58,11 +60,36 @@ class BridgePriceEntity(db.Model):
         """Gets the special price of the object."""
         return self.special_price
 
-    def set_special_price(self, special_price):
-        """Sets the special price of the object."""
-        if special_price is not None and special_price < 0:
-            raise ValueError("Special price cannot be negative.")
-        self.special_price = special_price
+    def set_special_price(self, special_price=None, percentage=None):
+        """
+        Sets the special price for the entity.
+
+        Either a special price or a percentage must be provided. If a percentage is provided,
+        the method will calculate the discount based on the original price.
+
+        :param special_price: The special price to be set.
+        :param percentage: The discount percentage for the price.
+        :raises ValueError: When neither or both price and percentage parameters are provided.
+        """
+        try:
+            # Check both input values are not provided
+            if special_price is not None and percentage is not None:
+                raise ValueError("Cannot set both special price and percentage. Please choose one.")
+
+            # Check neither of the input values are provided
+            if special_price is None and percentage is None:
+                raise ValueError("Either special_price or percentage must be provided.")
+
+            # Setting price directly if special_price is not None
+            if special_price is not None:
+                self.special_price = special_price
+            else:  # Calculating and setting price based on discount percentage
+                discount = (percentage / 100.0) * self.get_price()
+                self.special_price = self.get_price() - discount
+
+        except Exception as e:
+            print("Error occurred when calculating the special price:", str(e))
+            # Here you may add any additional exception handling or logging of your choice
 
     # Getter and Setter for special_start_date
     def get_special_start_date(self):
@@ -99,6 +126,58 @@ class BridgePriceEntity(db.Model):
     def set_edited_at(self, edited_at):
         """Sets the last edited date of the object."""
         self.edited_at = edited_at
+
+    """
+    Special getter and setter
+    """
+
+    def is_special_price_active(self):
+        """
+        Checks if the special price is currently active.
+
+        The special price is considered active if it's set and the current date & time falls between
+        the start and end dates of the special price period.
+
+        Returns:
+        bool: True if the special price is set and is active, False otherwise.
+        """
+        # Check if special price is set
+        if not self.get_special_price():
+            return False
+
+        active = self.get_special_start_date() <= datetime.datetime.now() <= self.get_special_end_date()
+
+        return active
+
+    def get_current_best_price(self, order_amount=None):
+        """
+        Returns the current best price for a given order amount.
+
+        If a special price is currently active, it will return this special price.
+        If not, it checks if the rebate conditions are met (rebate quantity is greater than
+        or equal to order amount and there is a rebate price). If they are, it returns the
+        rebate price. If none of these conditions are met, it returns the regular price.
+
+        If order_amount is not supplied, it will either return the special price
+        (if it's currently valid) or the regular price.
+
+        Args:
+            order_amount (int, optional): The number of items in the order. Defaults to None.
+
+        Returns:
+            float: The current best price.
+        """
+        try:
+            if self.get_special_price() and self.is_special_price_active():
+                return self.get_special_price()
+            elif order_amount is not None and self.get_rebate_quantity() >= order_amount and self.get_rebate_price():
+                return self.get_rebate_price()
+            else:
+                return self.get_price()
+        except Exception as e:
+            self.logger.error(f"Error getting the best price: {str(e)}")
+            return None
+
 
     def update(self, bridge_entity_new):
         """
