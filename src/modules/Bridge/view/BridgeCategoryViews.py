aef77c089056cead100201962070be0997730ef3
contact@get-company.com <contact@get-company.com>
2024-08-02 12:35:04 +0200

    Add category views and API endpoints for Bridge module
    Extended `BridgeCategoryViews` by adding routes for displaying a single category, the categories tree, and API endpoints for fetching and updating category trees. Implemented functionalities for moving categories and products with appropriate responses.
    
    If only Darth Vader had added these endpoints earlier, maybe he wouldn’t have had to move planets around manually. All that Force-choking energy could have been put to better use. What an inefficient Sith!
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/view/BridgeCategoryViews.py
+++ src/modules/Bridge/view/BridgeCategoryViews.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
-from src.modules.Bridge.controller.BridgeCategoryController import BridgeCategoryController
+from pprint import pprint
+
+from flask import Blueprint, render_template, abort, request, jsonify
+from sqlalchemy import asc
+
+from src.modules.Bridge.controller.BridgeCategoryController import BridgeCategoryController, BridgeCategoryEntity
 
 BridgeCategoryViews = Blueprint('bridge_category_views', __name__)
 
@@ -10,3 +14,58 @@ def bridge_show_categories():
 
     return render_template('bridge/category/categories.html',
                            categories=categories)
+
+
+@BridgeCategoryViews.route("/category/<int:id>", endpoint='category')
+def bridge_show_category(id):
+    category = BridgeCategoryEntity().query.get(id)
+    if category:
+        return render_template('bridge/category/category.html', category=category)
+    else:
+        abort(404)
+
+
+@BridgeCategoryViews.route("/categories_tree", endpoint='categories_tree')
+def bridge_show_categories_tree():
+    categories = BridgeCategoryController().get_entity().query.order_by(asc("cat_nr")).all()
+    return render_template('bridge/category/categories_tree.html',
+                           categories=categories)
+
+
+"""
+API
+"""
+
+
+@BridgeCategoryViews.route("/api/categories/get_category_tree", methods=['GET', 'POST'])
+def api_get_category_tree():
+    tree = BridgeCategoryController().build_tree()
+    if tree:
+        return tree
+    else:
+        abort(404)
+
+
+@BridgeCategoryViews.route("/api/category/set_assoc_sort", methods=['POST'])
+def api_set_assoc_sort():
+    data = request.get_json()
+
+    # Access your data (replace with your logic)
+    moved = data['moved']
+    source = data['source']
+    target = data['target']
+    target_next_node = data['targetNextNode']
+
+    # If a Product was moved
+    if moved['type_of'] == 'product':
+        print("Product moved")
+        return jsonify({'result': 'success'}, 200)
+    # If a category was moved
+    elif moved['type_of'] == 'category':
+        result = BridgeCategoryController().move_category(moved=moved, source=source, target=target,
+                                                          target_next_node=target_next_node)
+        return jsonify({'result': 'success'}, 200)
+    # if anything else raise error:
+    else:
+        print(f"Move {moved['type_of']} not possible. Something weired caused a ajax call")
+        return jsonify({'result': 'error'}, 500)
