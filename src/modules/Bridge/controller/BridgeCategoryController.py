aa6bae2974a6caadb1ebc206553c71a295f76f29
contact@get-company.com <contact@get-company.com>
2024-08-02 12:34:07 +0200

    Add category tree building and category moving functionality
    Implemented a recursive function to build a category tree with sub-categories and products. Added a method to handle the reordering of categories, ensuring proper updates to the `after_category_id` field of involved entities.
    
    If only the Empire had this level of detail in organizing their troops. Maybe they would have moved the Death Star out of the Rebel's attack range.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/controller/BridgeCategoryController.py
+++ src/modules/Bridge/controller/BridgeCategoryController.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+
+from sqlalchemy import asc, text, case
+
 from .BridgeAbstractController import BridgeAbstractController
 from ..entities.BridgeCategoryEntity import BridgeCategoryEntity
 import json
@@ -20,6 +24,79 @@ class BridgeCategoryController(BridgeAbstractController):
             return main_category
         else:
             return None
+
+    def build_tree(self, parent_id=None):
+        """ Recursive function to build category tree. """
+        tree = []
+
+        # Fetch categories with the given parent_id and order them by after_category_id
+        categories = (BridgeCategoryEntity.query
+                      .filter_by(parent_category_id=parent_id)
+                      .order_by(asc(BridgeCategoryEntity.after_category_id))
+                      .all())
+
+        for category in categories:
+            # Add category node
+            node = {
+                'data': {
+                    'type_of': 'category',
+                    'id': category.id,
+                    'after_category_id': category.after_category_id,
+                    'parent_category_id': category.parent_category_id,
+                    'title': category.get_translation().get_name(),
+                    'sw6_id': category.sw6_id,
+                },
+                'nodes': []  # Initialize nodes as empty, products or sub-categories will be added next
+            }
+
+            # Add related products as nodes under the category
+            if category.products:
+                for product in category.products:
+                    prod_cat_assoc = category.get_prod_cat_assoc(product)
+
+                    product_node = {
+                        'data': {
+                            'id': product.get_id(),
+                            'type_of': 'product',
+                            'title': product.get_translation().get_name(),
+                            # Replace with your method to get product name
+                            'category_id': category.get_id(),  # Assuming we use the same ERP number
+                            'sort': prod_cat_assoc.get_sort(),
+                        },
+                        'nodes': []  # Products don't have child nodes
+                    }
+                    node['nodes'].append(product_node)  # Add product nodes under the category node
+
+            # Recursive call for child categories and add them under the category node
+            node['nodes'].extend(self.build_tree(category.id))
+
+            # Add the fully constructed category node to the tree
+            tree.append(node)
+
+        return tree
+
+    def move_category(self, moved, source, target, target_next_node):
+        print("Moved:")
+        pprint(moved)
+        print("Source:")
+        pprint(source)
+        print("Target")
+        pprint(target)
+        print("Target Next Node:")
+        pprint(target_next_node)
+
+        moved_entity = self.get_entity().query.get(moved['id'])
+        # To move a category element, we need to do 3 things:
+        # 1. Set the after_category_id of the former next element
+        old_next_entity = BridgeCategoryEntity.query.filter_by(after_category_id=moved['id']).first()
+        print("Old Next Entity:")
+        pprint(old_next_entity)
+        # 2. Set the after_category_id of the moved element
+        # 3. Set the after_category_id of the new next element
+        return True
+
+    def __repr__(self):
+        return f'Bridge Category Entity ID: {self.id} - ERPNr: {self.erp_nr} - CatNr: {self.cat_nr}'
 
     def import_new_erp_ids(self):
         df = pd.read_excel("D:\\htdocs\\python\\GC-Bridge_v2\\categories_mix.xlsx")
@@ -44,3 +121,26 @@ class BridgeCategoryController(BridgeAbstractController):
 
         return True
 
+    def sync_all_to_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
+
+    def sync_one_to_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
+
+    def sync_changed_to_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
+
+    def sync_changed_from_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
+
+    def sync_all_from_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
+
+    def sync_one_from_bridge(self):
+        print("No Categories in ERP. Bridge is handling all of them!")
+        return True
