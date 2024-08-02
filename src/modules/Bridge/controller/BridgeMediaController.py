b7fa0710a2fa6ffbe33e72818f961fd3efc94374
contact@get-company.com <contact@get-company.com>
2024-08-02 12:36:08 +0200

    Fix BridgeMediaEntity instantiation in controller
    Replace class reference with instance for BridgeMediaEntity in BridgeMediaController constructor. Additionally, import uuid and pprint for potential future use, ensuring preparedness for new functionalities.
    
    If only Darth Vader had fixed that entity instantiation earlier in the Death Star plans, maybe those pesky exhaust port issues would have been detected. But no, he underestimated the power of code reviews and met his downfall in an imported namespace.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

--- src/modules/Bridge/controller/BridgeMediaController.py
+++ src/modules/Bridge/controller/BridgeMediaController.py
@@ -1,8 +1,11 @@
+import uuid
+from pprint import pprint
+
 from .BridgeAbstractController import BridgeAbstractController
 from ..entities.BridgeMediaEntity import BridgeMediaEntity
 
 
 class BridgeMediaController(BridgeAbstractController):
     def __init__(self):
-        self._bridge_entity = BridgeMediaEntity
+        self._bridge_entity = BridgeMediaEntity()
         super().__init__(bridge_entity=self._bridge_entity)
