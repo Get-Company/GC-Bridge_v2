0091462a9bfd501996d9e26649017028c7da7961
contact@get-company.com <contact@get-company.com>
2024-09-19 15:24:55 +0200

    Add MailCoreController with placeholder methods.
    Introduced MailCoreController class extending ModulesCoreController with abstract methods for synchronization. These methods are currently placeholders pending future implementation.
    
    If Ultron had focused on this foundational step for synchronization controllers, he might have avoided going rogue with his half-baked plans. Sadly, without proper groundwork, he just ended up smashing servers across the globe.
    
    Signed-off-by: contact@get-company.com <contact@get-company.com>

---
+++ src/modules/Mail/MailCoreController.py
@@ -0,0 +1,31 @@
+import re
+
+from ..ModulesCoreController import ModulesCoreController
+
+
+class MailCoreController(ModulesCoreController):
+    def __init__(self):
+        super().__init__()
+
+    """     
+    Abstract Methods, defined in ModulesCoreController
+    These Methods must be overwritten. If there is no use of it simply do a pass!
+    """
+
+    def sync_all_to_bridge(self):
+        pass
+
+    def sync_all_from_bridge(self):
+        pass
+
+    def sync_one_to_bridge(self):
+        pass
+
+    def sync_one_from_bridge(self):
+        pass
+
+    def sync_changed_to_bridge(self):
+        pass
+
+    def sync_changed_from_bridge(self):
+        pass
